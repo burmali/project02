148044aaab2b8e8c164bea462b8c5527e7e5bae4
Demet Buyuk <73418519+burmali@users.noreply.github.com>
2020-11-08 11:32:41 -0600

    fifth commit from remote master branch

--- src/firstpackage/FirstClass.java
+++ src/firstpackage/FirstClass.java
@@ -7,13 +7,13 @@ public class FirstClass {
 		System.out.println("first commit from local master branch");
 		System.out.println("second commit from local master");
 		System.out.println("Third commit from local master");
-		
-	        System.out.println("Forth commit");
+		System.out.println("Forth commit");
                 System.out.println("First commit");
 		System.out.println("Second commit");
 		System.out.println("Third commit")
+		System.out.println("here is the conflict  have from remote master branch");
 		System.out.println("Third commit")
-		System.out.println("hrere is the conflict  have from remote master branch");
+	        System.out.println("here is another conflict   from remote master branch");
 	}
 
 }
